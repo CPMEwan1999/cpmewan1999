9385d8055c24e65f997198e9a8fb935bc65a49cb
CPMEwan1999 <ahmad052ali@gmail.com>
2025-01-19 16:02:49 +0000

    Update cpmewan1999.py

--- cpmewan1999.py
+++ cpmewan1999.py
@@ -219,12 +219,3 @@ class CPMEwan1999:
         )
         response_decoded = response.json()
         return response_decoded.get("ok")
-        
-            def set_player_plates(self):
-        payload = { "account_auth": self.auth_token }
-        params = { "key": self.access_key }
-        response = requests.post(
-            f"{_MYAPI_URL_}/set_plates", params=params, data=payload
-        )
-        response_decoded = response.json()
-        return response_decoded.get("ok")
