112f83477e328633e5b9d57e56301d2f0a5ab974
CPMEwan1999 <ahmad052ali@gmail.com>
2025-01-20 22:42:09 +0000

    Update main.py

--- main.py
+++ main.py
@@ -606,20 +606,19 @@ if __name__ == "__main__":
                     
                     
                     
-            elif service == 22: # Hack Car Speed (299hp)
-                print(Colorate.Horizontal(Colors.rainbow, '[!] NOTE: ORIGINAL SPEED CAN NOT BE RESTORED'))
-                console.print("[%] HACKING CAR SPEED: ", end=None)
-                if cpm.hack_car_speed(car_id):
+            elif service == 22: # Unlock House 3
+                console.print("[%] UNLOCKING HOUSE 3: ", end=None)
+                if cpm.hack_car_speed():
                     print(Colorate.Horizontal(Colors.rainbow, 'SUCCESSFUL'))
-                    console.print("==================================")
+                    print(Colorate.Horizontal(Colors.rainbow, '======================================'))
                     answ = Prompt.ask("[?] DO YOU WANT TO EXIT ?", choices=["y", "n"], default="n")
-                    if answ == "y": console.print("[!] THANK YOU FOR USING OUR TOOL")
+                    if answ == "y": print(Colorate.Horizontal(Colors.rainbow, f'THANK YOU FOR USING OUR TOOL, PLEASE JOIN OUR TELEGRAM CHANNEL: @{__CHANNEL_USERNAME__}'))
                     else: continue
                 else:
                     print(Colorate.Horizontal(Colors.rainbow, 'FAILED'))
-                    print(Colorate.Horizontal(Colors.rainbow, '[!] PLEASE TRY AGAIN'))
+                    print(Colorate.Horizontal(Colors.rainbow, 'PLEASE TRY AGAIN'))
                     sleep(2)
-                    continue                    
+                    continue
                     
                     
                     
