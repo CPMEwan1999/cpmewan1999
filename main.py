7608eba38f7e9a08e244cc950434be862c2baa10
CPMEwan1999 <ahmad052ali@gmail.com>
2025-01-21 17:45:05 +0000

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ import logging
 import time
 import datetime
 
-from cryptography import x509
+
 from cryptography.exceptions import InvalidSignature
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization,hashes
