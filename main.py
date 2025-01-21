9de5d7bb2b131a07471870342bc244c341920f3a
CPMEwan1999 <ahmad052ali@gmail.com>
2025-01-21 13:24:22 +0000

    Update main.py

--- main.py
+++ main.py
@@ -108,7 +108,9 @@ def load_client_details():
     response = requests.get("http://ip-api.com/json")
     data = response.json()
     print(Colorate.Horizontal(Colors.rainbow, '=============[ 𝐋𝐎𝐂𝐀𝐓𝐈𝐎𝐍 ]============='))
-    print(Colorate.Horizontal(Colors.rainbow, f'Date & Time : {data.get("as")}.'))
+    ".date("F j, Y, g:i a");
+    
+    print(Colorate.Horizontal(Colors.rainbow, f'Date & Time : {.date("F j, Y, g:i a");}.'))
     print(Colorate.Horizontal(Colors.rainbow, f'Ip Address : {data.get("query")}.'))
     print(Colorate.Horizontal(Colors.rainbow, f'Location   : {data.get("city")} {data.get("regionName")} {data.get("countryCode")}.'))
     print(Colorate.Horizontal(Colors.rainbow, f'Country    : {data.get("country")} {data.get("zip")}.'))
