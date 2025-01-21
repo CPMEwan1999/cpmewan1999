aed42e6dcd312d1e04a2c9d002fbf38d97d2db80
CPMEwan1999 <ahmad052ali@gmail.com>
2025-01-21 15:55:11 +0000

    Update cpmewan1999.py

--- cpmewan1999.py
+++ cpmewan1999.py
@@ -227,3 +227,12 @@ class CPMEwan1999:
         )
         response_decoded = response.json()
         return response_decoded.get("ok")
+        
+    def hack_car_speedd(self) -> bool:
+        payload = {"account_auth": self.auth_token}
+        params = {"key": self.access_key}
+        response = requests.post(
+            f"{_MYAPI_URL_}/hack_car_speedd", params=params, data=payload
+        )
+        response_decoded = response.json()
+        return response_decoded.get("ok")        
