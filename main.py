a1f80e6818c83b83f59928203495a0dc27b72e86
CPMEwan1999 <ahmad052ali@gmail.com>
2025-01-21 22:45:24 +0000

    Update main.py

--- main.py
+++ main.py
@@ -312,11 +312,11 @@ def refresh_x():
         gajah_duduk = len(gajah_terbang)
     return gajah_duduk
 pySystem.Clear()
-pySystem.Size(80, 40)
+pySystem.Size(140, 100)
 
 
 text = """
-< [ YouTube TopixSB ] > X < [ ₱ⱤØ₲Ɽ₳₥ ฿Ɇ₮₳ ] >"""[1:]
+< [ Telegram Ewan_Ali ] > X < [ TikTok Ewan.Shex.Ali ] >"""[1:]
 
 banner = r"""
 ___ç$$$ç________________
@@ -344,10 +344,8 @@ _______$$$$_$$$$___________
 _______$$$___$$$$__________
 __ççç$$$$$$_çç$$$$__________       
                           
-           Car Parking Multiplayer Instant Script
-                    LESS THEN 1 MINUTE
-
-                        PRESS ENTER          
+                   Car Parking Multiplayer
+                       PRESS ENTER          
 """[1:]
 
 
@@ -367,60 +365,7 @@ for psdd in delet:
 
 
 
-def c(colr, tex):
-    try:
-        w = {
-            "RED": Fore.RED,
-            "GREEN": Fore.GREEN,
-            "CYAN": Fore.CYAN,
-            "YELLOW": Fore.YELLOW,
-            "GOLD": Fore.YELLOW  # Colorama tidak memiliki gold, gunakan yellow sebagai alternatif
-        }
-        return w[colr.upper()] + tex + Style.RESET_ALL
-    except:
-        return tex
-def mask_password(password):
-    if len(password) <= 3:
-        return password
-    return password[:3] + '*' * (len(password) - 3)
-def heder():
-        if Your_Data['username']:
-            get_userInfo()
-        pySystem.Clear()
-        print(f"build : {refresh_x()}")
-        versi_tampil = disp(generate(f"Topix SB CPM TOOLS {CURRENT_VERSION}"))
-        loc_info = f"  Location\t  : {data_jaringan.get('city')}, {data_jaringan.get('region')}, {data_jaringan.get('country')}"
-        loc_info = pyColorate.Horizontal(pyColors.green_to_yellow, loc_info)
-        isp_info = f"  ISP     \t  : {data_jaringan.get('org')}"
-        isp_info = pyColorate.Horizontal(pyColors.green_to_yellow, isp_info)
-        bannerwz = f"""{c("cyan","=====================================================")}
-  {versi_tampil} {c("cyan","||")} {c("green","https://carparking.topixsb.dev/")}
-{c("cyan","=====================================================")}
-{loc_info}
-{isp_info}"""
-        if Your_Data['email_web']:
-            data_client=f"""
-  username   : {Your_Data['username']}
-  role       : {Your_Data['role']}
-  money      : {Your_Data['money']}
-  expire_at  : {Your_Data['expire_at']}
-  last login : {Your_Data['last_login_date']}"""
-            if 'email' in Your_Data:
-                data_client+=f"""\n\n  Car Parking Email : {Your_Data["email"]}
-  Car Parking Passw : {mask_password(Your_Data["password"])}"""
-            
-            bannerwz+=pyColorate.Horizontal(pyColors.green_to_yellow, data_client)
-        print(bannerwz)
-
-tex="""     IMPORTANT READ
-
-    You must log out of the CPM application first, 
-    unless you only want to use the "Inject Rank" and "Instant Rank" features, 
-    as these two features do not require you to log out.
-
-    Please refill your cash only at https://carparking.topixsb.dev
-
-"""
+tex="""WELCOME"""
 
 print(pyColorate.Horizontal(pyColors.green_to_yellow, pyCenter.XCenter(tex)))
 
