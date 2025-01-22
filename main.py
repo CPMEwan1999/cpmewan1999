dcb85fe9c67e5224a6af6f288f1fe0d928ae7b30
CPMEwan1999 <ahmad052ali@gmail.com>
2025-01-22 19:10:27 +0000

    Update main.py

--- main.py
+++ main.py
@@ -149,7 +149,13 @@ pySystem.Clear()
 
 
 
+name = input("What is your name?: ")
+age = int(input("How old are you?: "))
+height = float(input("How tall are you?: "))
 
+print("Hello "+name)
+print("You are "+str(age)+" years old")
+print("You are "+str(height)+"cm tall")
 
 
 
