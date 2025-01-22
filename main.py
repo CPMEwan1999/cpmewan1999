4175a2fc6645b3852787287929d162f77dc22c9a
CPMEwan1999 <ahmad052ali@gmail.com>
2025-01-22 18:39:32 +0000

    Update main.py

--- main.py
+++ main.py
@@ -269,7 +269,7 @@ def load_client_details():
     data = response.json()
     print(Colorate.Horizontal(Colors.rainbow, '==============[ 𝐋𝐎𝐂𝐀𝐓𝐈𝐎𝐍 ]=============='))
     now = datetime.datetime.now()
-    print(Colorate.Horizontal(Colors.rainbow, (now.strftime("📍DateTime: %Y-%m-%d %H:%M:%S"))))
+    print(Colorate.Horizontal(Colors.rainbow, (now.strftime("📍DateTime: %d-%m-%Y %H:%M:%S"))))
     print(Colorate.Horizontal(Colors.rainbow, f'📍Country : {data.get("country")}.'))    
     print(Colorate.Horizontal(Colors.rainbow, f'📍Region  : {data.get("regionName")}.'))
     print(Colorate.Horizontal(Colors.rainbow, f'📍City    : {data.get("city")}.'))
