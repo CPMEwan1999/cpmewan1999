a74f0ae210f63c24a16faf213a08ea604d20b5ac
CPMEwan1999 <ahmad052ali@gmail.com>
2025-01-22 22:26:37 +0000

    Update main.py

--- main.py
+++ main.py
@@ -134,8 +134,7 @@ banner = r"""
                          PRESS ENTER                                 
 """[1:]
 
-
-pyAnime.Fade(pyCenter.Center(banner), pyColors.red_to_yellow, pyColorate.Vertical, enter=True)
+pyAnime.Fade(Center.XCenter, pyCenter.Center(banner), pyColors.red_to_yellow, pyColorate.Vertical, enter=True)
 
 
 #pyAnime.Fade(pyCenter.Center(text), pyColors.purple_to_red, pyColorate.Vertical, enter=True)
@@ -150,6 +149,15 @@ pySystem.Clear()
 
 
 
+
+#پرسیار و وەڵام 
+#name = input("What is your name?: ")
+#age = int(input("How old are you?: "))
+#height = float(input("How tall are you?: "))
+
+#print("Hello "+name)
+#print("You are "+str(age)+" years old")
+#print("You are "+str(height)+"cm tall")
 
 
 
