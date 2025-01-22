7a93e4366eebde02de291b5b80e385a66f1cc94f
CPMEwan1999 <ahmad052ali@gmail.com>
2025-01-22 15:04:32 +0000

    Update main.py

--- main.py
+++ main.py
@@ -136,10 +136,6 @@ banner = r"""
 
 
 pyAnime.Fade(pyCenter.Center(banner), pyColors.red_to_yellow, pyColorate.Vertical, enter=True)
-print(pyColorate.Horizontal(pyColors.purple_to_red, pyCenter.XCenter(banner)))
-print(pyColorate.Horizontal(pyColors.red_to_yellow, pyCenter.XCenter(banner)))
-print(pyColorate.Horizontal(pyColors.purple_to_red, pyCenter.XCenter(banner)))
-print(pyColorate.Horizontal(pyColors.red_to_yellow, pyCenter.XCenter(banner)))
 
 
 #pyAnime.Fade(pyCenter.Center(text), pyColors.purple_to_red, pyColorate.Vertical, enter=True)
