5ecb725c1eb279689b87be5f6cf8e19bb23358f8
CPMEwan1999 <ahmad052ali@gmail.com>
2025-01-22 12:46:33 +0000

    Update main.py

--- main.py
+++ main.py
@@ -139,7 +139,8 @@ banner = r"""
 """[1:]
 
 
-pyAnime.Fade(pyCenter.Center(banner), pyColors.red_to_yellow_to_blue, pyColorate.Vertical, enter=True)
+pyAnime.Fade(pyCenter.Center(banner), pyColors.red_to_yellow, pyColorate.Vertical, enter=True)
+pyAnime.Fade(pyCenter.Center(text), pyColors.purple_to_red, pyColorate.Vertical, enter=True)
 pyAnime.Fade(pyCenter.Center(text), pyColors.purple_to_red, pyColorate.Vertical, enter=True)
 
 pySystem.Clear()
