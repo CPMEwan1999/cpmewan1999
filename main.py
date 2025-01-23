d5bf8230e807f06823e22fd72f8be5b49d710333
CPMEwan1999 <ahmad052ali@gmail.com>
2025-01-23 12:32:12 +0000

    Update main.py

--- main.py
+++ main.py
@@ -302,77 +302,6 @@ def rainbow_gradient_string(customer_name):
     return modified_string
 
 
-
-
-# Python Banking Program
-
-def show_balance(balance):
-    print("*********************")
-    print(f"Your balance is ${balance:.2f}")
-    print("*********************")
-
-def deposit():
-    print("*********************")
-    amount = float(input("Enter an amount to be deposited: "))
-    print("*********************")
-    if amount < 0:
-        print("*********************")
-        print("That's not a valid amount")
-        print("*********************")
-        return 0
-    else:
-        return amount
-
-def withdraw(balance):
-    print("*********************")
-    amount = float(input("Enter amount to be withdrawn: "))
-    print("*********************")
-
-    if amount > balance:
-        print("*********************")
-        print("Insufficient funds")
-        print("*********************")
-        return 0
-    elif amount < 0:
-        print("*********************")
-        print("Amount must be greater than 0")
-        print("*********************")
-        return 0
-    else:
-        return amount
-
-def main():
-    balance = 0
-    is_running = True
-
-    while is_running:
-        print("*********************")
-        print("   Banking Program   ")
-        print("*********************")
-        print("1.Show Balance")
-        print("2.Deposit")
-        print("3.Withdraw")
-        print("4.Exit")
-        print("*********************")
-        choice = input("Enter your choice (1-4): ")
-
-        if choice == '1':
-            show_balance(balance)
-        elif choice == '2':
-            balance += deposit()
-        elif choice == '3':
-            balance -= withdraw(balance)
-        elif choice == '4':
-            is_running = False
-        else:
-            print("*********************")
-            print("That is not a valid choice")
-            print("*********************")
-
-    print("*********************")
-    print("Thank you! Have a nice day!")
-    print("*********************")
-
 if __name__ == "__main__":
     console = Console()
     signal.signal(signal.SIGINT, signal_handler)
@@ -453,17 +382,6 @@ if __name__ == "__main__":
                     if cpm.set_player_money(amount):
                         print(Colorate.Horizontal(Colors.rainbow, 'SUCCESSFUL'))
                         print(Colorate.Horizontal(Colors.rainbow, '======================================'))
-                        answ = Prompt.ask("[?] DO YOU WANT TO EXIT ?", choices=["y", "n"], default="n")
-                        if answ == "y": print(Colorate.Horizontal(Colors.rainbow, f'THANK YOU FOR USING OUR TOOL, PLEASE JOIN OUR TELEGRAM CHANNEL: @{__CHANNEL_USERNAME__}'))
-                        else: continue
-                    else:
-                        print(Colorate.Horizontal(Colors.rainbow, 'FAILED'))
-                        print(Colorate.Horizontal(Colors.rainbow, 'PLEASE TRY AGAIN'))
-                        sleep(2)
-                        continue
-                else:
-                    print(Colorate.Horizontal(Colors.rainbow, 'FAILED'))
-                    print(Colorate.Horizontal(Colors.rainbow, 'PLEASE USE VALID VALUES'))
                     sleep(2)
                     continue
             elif service == 2: # Increase Coins
