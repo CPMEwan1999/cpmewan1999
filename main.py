6a0c150c2decdb3e959b66f55a338ee50d0e8e14
CPMEwan1999 <ahmad052ali@gmail.com>
2025-01-23 21:29:07 +0000

    Update main.py

--- main.py
+++ main.py
@@ -339,7 +339,7 @@ if __name__ == "__main__":
             load_player_data(cpm)
             load_key_data(cpm)
             load_client_details()
-            choices = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24"]
+            choices = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25"]
             print(Colorate.Horizontal(Colors.rainbow, '📍{01}: Increase Money           1.000K'))
             print(Colorate.Horizontal(Colors.rainbow, '📍{02}: Increase Coins           3.500K'))
             print(Colorate.Horizontal(Colors.rainbow, '📍{03}: King Rank                4.000K'))
@@ -363,7 +363,8 @@ if __name__ == "__main__":
             print(Colorate.Horizontal(Colors.rainbow, '📍{21}: Change Race Loses        1.000K'))
             print(Colorate.Horizontal(Colors.rainbow, '📍{22}: Speed Car Hack           2.500K'))
             print(Colorate.Horizontal(Colors.rainbow, '📍{23}: All Cars 99HP            2.500K'))
-            print(Colorate.Horizontal(Colors.rainbow, '📍{24}: Clone Account            5.000K'))            
+            print(Colorate.Horizontal(Colors.rainbow, '📍{24}: Chrome All Cars            2.000K'))            
+            print(Colorate.Horizontal(Colors.rainbow, '📍{25}: Clone Account            5.000K'))            
             print(Colorate.Horizontal(Colors.rainbow, '📍{00} : Exit'))
             
             print(Colorate.Horizontal(Colors.rainbow, '================[ 𝐂𝐏𝐌☆ ]================'))
@@ -743,6 +744,20 @@ if __name__ == "__main__":
                     print(Colorate.Horizontal(Colors.rainbow, 'PLEASE TRY AGAIN'))
                     sleep(2)
                     continue
+                            elif service == 24: # Chrome All Cars
+                print(Colorate.Horizontal(Colors.rainbow, '[!] NOTE: ORIGINAL SPEED CAN NOT BE RESTORED'))            
+                console.print("[%] HACKING All CARS SPEED 99HP: ", end=None)
+                if cpm.chrome_all_cars():
+                    print(Colorate.Horizontal(Colors.rainbow, 'SUCCESSFUL'))
+                    print(Colorate.Horizontal(Colors.rainbow, '======================================'))
+                    answ = Prompt.ask("[?] DO YOU WANT TO EXIT ?", choices=["y", "n"], default="n")
+                    if answ == "y": print(Colorate.Horizontal(Colors.rainbow, f'THANK YOU FOR USING OUR TOOL, PLEASE JOIN OUR TELEGRAM CHANNEL: @{__CHANNEL_USERNAME__}'))
+                    else: continue
+                else:
+                    print(Colorate.Horizontal(Colors.rainbow, 'FAILED'))
+                    print(Colorate.Horizontal(Colors.rainbow, 'PLEASE TRY AGAIN'))
+                    sleep(2)
+                    continue    
             elif service == 24: # Clone Account
                 print(Colorate.Horizontal(Colors.rainbow, '[!] PLEASE ENTER ACCOUNT DETALIS'))
                 to_email = prompt_valid_value("[?] ACCOUNT EMAIL", "Email", password=False)
