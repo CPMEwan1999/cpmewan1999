4cd5dd54e654b8ec7e6dae69dec21d6acda540f6
CPMEwan1999 <ahmad052ali@gmail.com>
2025-01-26 22:06:40 +0000

    Update main.py

--- main.py
+++ main.py
@@ -218,7 +218,7 @@ def banner(console):
     ]
     colorful_text = gradient_text(brand_name, colors)
     console.print(colorful_text)
-print(Box.Lines("================================================================================="))
+    print(Colorate.Horizontal(Colors.rainbow, '================================================================================='))
     print(Colorate.Horizontal(Colors.rainbow, Center.XCenter("𝐏𝐋𝐄𝐀𝐒𝐄 𝐋𝐎𝐆𝐎𝐔𝐓 𝐅𝐑𝐎𝐌 𝐂𝐏𝐌 𝐁𝐄𝐅𝐎𝐑𝐄 𝐔𝐒𝐈𝐍𝐆 𝐓𝐇𝐈𝐒 𝐓𝐎𝐎𝐋")))
     
     print(Colorate.Horizontal(Colors.rainbow, Center.XCenter("𝐒𝐇𝐀𝐑𝐈𝐍𝐆 𝐓𝐇𝐄 𝐀𝐂𝐂𝐄𝐒𝐒 𝐊𝐄𝐘 𝐈𝐒 𝐍𝐎𝐓 𝐀𝐋𝐋𝐎𝐖𝐄𝐃 𝐀𝐍𝐃 𝐖𝐈𝐋𝐋 𝐁𝐄 𝐁𝐋𝐎𝐂𝐊𝐄𝐃")))
@@ -226,7 +226,7 @@ print(Box.Lines("===============================================================
     print(Colorate.Horizontal(Colors.rainbow, Center.XCenter(f" 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦: @{__CHANNEL_USERNAME__} 𝐎𝐫 @{__GROUP_USERNAME__}")))
     print(Colorate.Horizontal(Colors.rainbow, Center.XCenter(Box.DoubleCube("Hello, Welcome to Pystyle."))))
     
-    print(Colorate.Horizontal(Colors.rainbow, '================================================================================='))
+print(Box.Lines("================================================================================="))
 
 def load_player_data(cpm):
     response = cpm.get_player_data()
