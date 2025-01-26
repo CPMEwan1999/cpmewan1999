c47aa1a10220f7c5335190af35f8655dc1efc5d8
CPMEwan1999 <ahmad052ali@gmail.com>
2025-01-26 22:07:29 +0000

    Update main.py

--- main.py
+++ main.py
@@ -226,7 +226,9 @@ def banner(console):
     print(Colorate.Horizontal(Colors.rainbow, Center.XCenter(f" 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦: @{__CHANNEL_USERNAME__} 𝐎𝐫 @{__GROUP_USERNAME__}")))
     print(Colorate.Horizontal(Colors.rainbow, Center.XCenter(Box.DoubleCube("Hello, Welcome to Pystyle."))))
     
-print(Box.Lines("================================================================================="))
+    print(Box.Lines("Hello, Welcome to Pystyle."))
+    
+    print(Colorate.Horizontal(Colors.rainbow, '================================================================================='))
 
 def load_player_data(cpm):
     response = cpm.get_player_data()
