3a83342a4337c4b0821967ca06cb785b3b9e19ec
CPMEwan1999 <ahmad052ali@gmail.com>
2025-01-26 22:44:42 +0000

    Update main.py

--- main.py
+++ main.py
@@ -256,7 +256,7 @@ def load_key_data(cpm):
 
     data = cpm.get_key_data()
     
-    print(Colorate.Horizontal(Colors.rainbow, '─════════[ 𝙳𝙴𝚃𝙰𝙸𝙻𝚂 ]════════─'))
+    print(Colorate.Horizontal(Colors.rainbow, '─═════════════[ 𝙳𝙴𝚃𝙰𝙸𝙻𝚂 ]═════════════─'))
     
     print(Colorate.Horizontal(Colors.rainbow, f'📍Access Key : {data.get("access_key")}.'))
     
