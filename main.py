41b80ef505c1920ea40092109a5b9984ac2781fb
CPMEwan1999 <ahmad052ali@gmail.com>
2025-01-26 21:59:50 +0000

    Update main.py

--- main.py
+++ main.py
@@ -224,7 +224,7 @@ def banner(console):
     print(Colorate.Horizontal(Colors.rainbow, Center.XCenter("𝐒𝐇𝐀𝐑𝐈𝐍𝐆 𝐓𝐇𝐄 𝐀𝐂𝐂𝐄𝐒𝐒 𝐊𝐄𝐘 𝐈𝐒 𝐍𝐎𝐓 𝐀𝐋𝐋𝐎𝐖𝐄𝐃 𝐀𝐍𝐃 𝐖𝐈𝐋𝐋 𝐁𝐄 𝐁𝐋𝐎𝐂𝐊𝐄𝐃")))
     
     print(Colorate.Horizontal(Colors.rainbow, Center.XCenter(f" 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦: @{__CHANNEL_USERNAME__} 𝐎𝐫 @{__GROUP_USERNAME__}")))
-    print(Colorate.Horizontal(Colors.rainbow, Center.XCenter, Box.DoubleCube("Hello, Welcome to Pystyle.")))
+    print(Colorate.Horizontal(Colors.rainbow(Box.DoubleCube("Hello, Welcome to Pystyle."))))
     
     print(Colorate.Horizontal(Colors.rainbow, '================================================================================='))
 
