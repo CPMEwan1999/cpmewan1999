6f2b011aa1d451ca32cb3124f940df89cd878abd
CPMEwan1999 <ahmad052ali@gmail.com>
2025-01-27 17:06:14 +0000

    Update main.py

--- main.py
+++ main.py
@@ -238,11 +238,7 @@ def load_player_data(cpm):
             
             print(Colorate.Horizontal(Colors.rainbow, f'📍Name   : {(data.get("Name") if "Name" in data else "UNDEFINED")}.'))
                 
-            print(Colorate.Horizontal(Colors.rainbow, f'📍LocalID: {data.get("localID")}.'))
-            
-            print(Colorate.Horizontal(Colors.rainbow, f'📍Money  : {data.get("money")}.'))
-            
-            print(Colorate.Horizontal(Colors.rainbow, f'📍Coins  : {data.get("coin")}.'))
+print(Colorate.Horizontal(Colors.rainbow, f'📍LocalID: {data.get("localID")} Money : {data.get("money")} : {data.get("coin")}'))
             
         else:
             print(Colorate.Horizontal(Colors.rainbow, '! ERROR: new accounts most be signed-in to the game at least once !.'))
