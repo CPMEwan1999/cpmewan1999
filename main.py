ef4c31c7fc52465c309c7253cc1c6eb1926d2c3e
CPMEwan1999 <ahmad052ali@gmail.com>
2025-01-28 23:00:18 +0000

    Update main.py

--- main.py
+++ main.py
@@ -330,7 +330,7 @@ if __name__ == "__main__":
             load_key_data(cpm)
             load_client_details()
             choices = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27"]
-            print(Colorate.Horizontal(Colors.rainbow, Center.XCenter(Box.DoubleCube( '{01}: Increase Money [1.000K] & {02}: Increase Coins [3.000K]\n{03}: King Rank [3.500K] & {04}: Change ID [2.500K]\n'))))
+            print(Colorate.Horizontal(Colors.rainbow, Center.XCenter(Box.DoubleCube( '{01}: Increase Money [1.000K] & {02}: Increase Coins [3.000K]\n     {03}: King Rank [3.500K] & {04}: Change ID [2.500K]\n'))))
             
             
 
