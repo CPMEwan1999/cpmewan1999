d52761043125c2bd32b4fdf8dd98d5f973d5e282
CPMEwan1999 <ahmad052ali@gmail.com>
2025-01-28 22:42:15 +0000

    Update main.py

--- main.py
+++ main.py
@@ -331,7 +331,7 @@ if __name__ == "__main__":
             load_client_details()
             choices = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27"]
             print(Colorate.Horizontal(Colors.rainbow, Center.XCenter(Box.DoubleCube( '{01}: Increase Money [1.000K] | {02}: Increase Coins [3.000K]'))))
-            print(Colorate.Horizontal(Colors.rainbow, Center.XCenter(Box.DoubleCube( '{03}: King Rank [3.500K] | {04}: Change ID [2.500K]'))
+            print(Colorate.Horizontal(Colors.rainbow, Center.XCenter(Box.DoubleCube( '{03}: King Rank [3.500K] | {04}: Change ID [2.500K]'))))
             print(Colorate.Horizontal(Colors.rainbow, '➩{05}: Change Name               1.00K'))
             print(Colorate.Horizontal(Colors.rainbow, '➩{06}: Change Name (Rainbow)     1.00K'))
             print(Colorate.Horizontal(Colors.rainbow, '➩{07}: Number Plates             2.000K'))
